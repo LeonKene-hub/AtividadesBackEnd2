0e9c18321a0a5e87947c0ad352693cc40c071fe7
Pedro H. Alves <leonkene18@gmail.com>
2023-04-13 10:59:00 -0300

    A

---
+++ Atividade 1/Program.cs
@@ -0,0 +1,18 @@
+﻿//data do sitema
+DateTime ano = DateTime.Today;
+
+//data de nascimento do usuario
+Console.WriteLine($"Por favor, informe seu ano de nascimo para informar se pode votar ou nao");
+int anoUsuario = int.Parse(Console.ReadLine());
+
+int idade = ano.Year - anoUsuario;
+Console.WriteLine($"{idade} anos de idade");
+
+if (idade == 16)
+{
+    Console.WriteLine($"O seu voto e opicional");
+}
+else if (idade >= 18)
+{
+    Console.WriteLine($"Seu voto e obrigatorio");
+}
---
+++ Atividade 2/Program.cs
@@ -0,0 +1,56 @@
+﻿//escolha entre alcool e gasolina
+Console.WriteLine(@$"
+Seja bem vindo, gostaria de encher o tanque com alcool ou gasolina?
+Digite A para acool ou G para gasolina
+");
+string escolha = Console.ReadLine().ToLower();
+//a ou g
+
+//quantidade
+Console.WriteLine($"Quantos litros?");
+double litros = double.Parse(Console.ReadLine());
+//--------------------------------------------------
+
+Console.WriteLine($"{PostoDeGasolina(escolha, litros)}");
+
+
+static double PostoDeGasolina(string escolha, double litros)
+{
+    //valor e o tipo escolhido
+    double valor = 0;
+
+    if (escolha == "a")
+    {
+        escolha = "alcool";
+        valor = 4.90f;
+    }
+    else if (escolha == "g")
+    {
+        escolha = "gasolina";
+        valor = 5.30f;
+    }
+
+    //quantidade e preco final
+    if (litros <= 20 && escolha == "alcool")
+    {
+        valor = Math.Round((litros * valor) * 0.97, 2);
+        return (valor);
+    }
+    else if (litros > 20 && escolha == "alcool")
+    {
+        valor = Math.Round((litros * valor) * 0.95, 2);
+        return (valor);
+    }
+    else if (litros <= 20 && escolha == "gasolina")
+    {
+        valor = Math.Round((litros * valor) * 0.96, 2);
+        return (valor);
+    }
+    else if (litros > 20 && escolha == "gasolina")
+    {
+        valor = Math.Round((litros * valor) * 0.94, 2);
+        return (valor);
+    }
+
+}
+
